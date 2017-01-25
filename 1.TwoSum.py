d9649afa2451661ceeebaf5675d4ac7d71b74d77
King26535 <king26535@163.com>
2017-01-25 09:23:53 +0800

    Update 1.TwoSum.py
    two pointers

--- 1.TwoSum.py
+++ 1.TwoSum.py
@@ -5,6 +5,7 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        '''
         l = len(nums)
         for i in range(l - 1):
             for j in range(i + 1, l):
@@ -12,3 +13,24 @@ class Solution(object):
                     m = i
                     n = j
         return [m, n]
+        '''
+        temp = nums[:]
+        temp.sort()
+        result = []
+        l,r = 0,len(temp)-1
+        while l<r:
+            if temp[l] + temp[r] == target:
+                for i in range(len(nums)):
+                    if temp[l] == nums[i]:
+                        result.append(i)
+                        break
+                for j in range(len(nums)-1,-1,-1):
+                    if temp[r] == nums[j]:
+                        result.append(j)
+                        break
+                break
+            elif temp[l] + temp[r] < target:
+                l += 1
+            else:
+                r -= 1
+        return result
