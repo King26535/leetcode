1726d7b8a38178686dc0b2d527cc721f4c8d4aba
King26535 <king26535@163.com>
2017-02-07 09:13:11 +0800

    Create 29.DivideTwoIntegers.py
    Divide two integers without using multiplication, division and mod operator.
    
    If it is overflow, return 2**31-1.
    -2**31 is ok,

---
+++ 29.DivideTwoIntegers.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        sign = False
+        if (dividend < 0 and divisor > 0) or (dividend > 0 and divisor < 0):
+            sign = True
+        
+        a,b,res = abs(dividend),abs(divisor),0
+        
+        if b == 0 or (a >= 2**31-1 and divisor == -1):
+            return 2**31-1
+
+        while a >= b:
+            temp,multiple = b,1
+            while a >= (temp << 1):
+                temp = temp << 1
+                multiple = multiple << 1
+            a -= temp
+            res += multiple
+        res = -res if sign else res
+        return res
